c3b21a25a58fb3582ebd031bbbd3113463f10b44
chenghuazhangnxwavegmail <126008410+chenghuazhangnxwavegmail@users.noreply.github.com>
2023-04-04 15:57:44 +0100

    Add files via upload

---
+++ uploads/packages/czsnestedcdt.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 03, 2023 at 11:05 AM
+-- Server version: 10.6.11-MariaDB-log
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `Appian`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `czsnestedcdt`
+--
+
+
+--
+-- Dumping data for table `czsnestedcdt`
+--
+
+INSERT INTO `czsnestedcdt` (`text`) VALUES
+('test external deployment');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `czsnestedcdt`
+--
+
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `czsnestedcdt`
+--
+
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
